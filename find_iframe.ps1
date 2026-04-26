a21eb4c979d1e21cc3b679cd7bd99467b3ca67f4
controlvilleda-star <control.villeda@gmail.com>
2026-04-26 23:56:15 +0200

    tu mensaje aquí

--- find_iframe.ps1
+++
@@ -1,8 +0,0 @@
-$3abn = Get-Content 3abn.html -Raw
-if ($3abn -match '<iframe.*?src=["\'](.*?)["\'].*?>') {
-    Write-Host "3ABN iframe: $($matches[1])"
-}
-$nt = Get-Content nt.html -Raw
-if ($nt -match '<iframe.*?src=["\'](.*?)["\'].*?>') {
-    Write-Host "NT iframe: $($matches[1])"
-}
